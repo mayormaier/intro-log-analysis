df883d70423bc5dd22e42cd2169ad108ff49fa08
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-11-08 15:32:56 -0500

    initialize

---
+++ analyze-better.py
@@ -0,0 +1,80 @@
+# ANDREW MAIER ADM5859 LOG ANALYSIS
+import http.client
+
+with open("Log-A.strace", "r") as log_a, open("Log-B.strace", "r") as log_b:
+    a_content = log_a.readlines()
+    b_content = log_b.readlines()
+
+    read_expression = " read("
+    keyboard_expression = "tty"
+    pipe_expression = "pipe"
+
+    def count_read_events(log, log_content):
+        print("counting read events from log", log)
+        count = 0
+        for line in log_content:
+            if read_expression in line:
+                print("Read event from Log", log, ":" + line)
+                count += 1
+        return count
+
+    def count_keyboard_read_events(log, log_content):
+        print("counting keyboard read events from log", log)
+        count = 0
+        for line in log_content:
+            if read_expression in line and keyboard_expression in line:
+                print("Keyboard Read Event from Log", log, ":" + line)
+                count += 1
+        return count
+
+    def count_file_read_events(log, log_content):
+        print("counting file read events from log", log)
+        count = 0
+        for line in log_content:
+            if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
+                print("File Read Event from Log", log, ":" + line)
+                count += 1
+        return count
+
+    def find_files_accessed(log, log_content):
+        filename_list = list()
+        for line in log_content:
+            if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
+                filename_start = line.find('<')
+                filename_end = line.find('>')
+                filename = str(line[filename_start + 1:filename_end])
+                filename_list.append(filename)
+        return filename_list
+
+    # Task 1
+    A_read_events = count_read_events("A", a_content)
+    B_read_events = count_read_events("B", b_content)
+
+    # Task 2
+    A_keyboard_events = count_keyboard_read_events("A", a_content)
+    B_keyboard_events = count_keyboard_read_events("B", b_content)
+
+    # Task 3
+    A_file_read_events = count_file_read_events("A", a_content)
+    B_file_read_events = count_file_read_events("B", b_content)
+
+    # Task 4
+    A_files_accessed = find_files_accessed("A", a_content)
+    B_files_accessed = find_files_accessed("B", b_content)
+
+    print(A_read_events, "read events in Log A")
+    print(B_read_events, "read events in Log B")
+
+    print(A_keyboard_events, "keyboard read events in Log A")
+    print(B_keyboard_events, "keyboard read events in Log B")
+
+    print(A_file_read_events, "file read events in Log A")
+    print(B_file_read_events, "file read events in Log B")
+
+    print("files accessed in log A:")
+    for file in A_files_accessed:
+        print(file)
+
+    print("files accessed in log B:")
+    for file in B_files_accessed:
+        print(file)
---
+++ analyze.py
@@ -0,0 +1,55 @@
+# ANDREW MAIER ADM5859 LOG ANALYSIS
+
+with open("Log-A.strace", "r") as log_a, open("Log-B.strace", "r") as log_b:
+    a_content = log_a.readlines()
+    b_content = log_b.readlines()
+
+    # Task 1
+    read_expression = " read("
+    count = 0
+    for line in a_content:
+        if read_expression in line:
+            # print("Read from Log A: " + line)
+            count += 1
+    print(str(count) + " read events in Log A")
+
+    count = 0
+    for line in b_content:
+        if read_expression in line:
+            # print("Read from Log B: " + line)
+            count += 1
+    print(str(count) + " read events in Log B")
+
+    # Task 2
+    keyboard_expression = "tty"
+    count = 0
+    for line in a_content:
+        if read_expression in line and keyboard_expression in line:
+            # print("Keyboard Read Event from Log A: " + line)
+            count += 1
+    print(str(count) + " keyboard read events in Log A")
+
+    count = 0
+    for line in b_content:
+        if read_expression in line and keyboard_expression in line:
+            # print("Keyboard Read Event from Log B: " + line)
+            count += 1
+    print(str(count) + " keyboard read events in Log B")
+
+    # Task 3
+    pipe_expression = "pipe"
+    count = 0
+    for line in a_content:
+        if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
+            # print("File read event from Log A: " + line)
+            count += 1
+    print(str(count) + " file read events in Log A")
+
+    count = 0
+    for line in b_content:
+        if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
+            # print("File read event from Log B: " + line)
+            count += 1
+    print(str(count) + " file read events in Log B")
+
+    # Task 4
