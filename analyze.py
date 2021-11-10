7a7f0b26ccedbbaf76b9ab040d5566423de4ceb6
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-11-09 21:28:30 -0500

    add output files

--- analyze.py
+++ analyze.py
@@ -9,16 +9,16 @@ with open("Log-A.strace", "r") as log_a, open("Log-B.strace", "r") as log_b:
     count = 0
     for line in a_content:
         if read_expression in line:
-            print("Read from Log A: " + line)
+            print("Read event from Log A: " + line)
             count += 1
-    print(str(count) + " read events in Log A")
+    a_read_events_count = count
 
     count = 0
     for line in b_content:
         if read_expression in line:
-            print("Read from Log B: " + line)
+            print("Read event from Log B: " + line)
             count += 1
-    print(str(count) + " read events in Log B")
+    b_read_events_count = count
 
     # Task 2
     keyboard_expression = "tty"
@@ -27,14 +27,14 @@ with open("Log-A.strace", "r") as log_a, open("Log-B.strace", "r") as log_b:
         if read_expression in line and keyboard_expression in line:
             print("Keyboard Read Event from Log A: " + line)
             count += 1
-    print(str(count) + " keyboard read events in Log A")
+    a_keyboard_read_events_count = count
 
     count = 0
     for line in b_content:
         if read_expression in line and keyboard_expression in line:
             print("Keyboard Read Event from Log B: " + line)
             count += 1
-    print(str(count) + " keyboard read events in Log B")
+    b_keyboard_read_events_count = count
 
     # Task 3
     pipe_expression = "pipe"
@@ -43,11 +43,18 @@ with open("Log-A.strace", "r") as log_a, open("Log-B.strace", "r") as log_b:
         if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
             print("File read event from Log A: " + line)
             count += 1
-    print(str(count) + " file read events in Log A")
+    a_file_read_events_count = count
 
     count = 0
     for line in b_content:
         if read_expression in line and keyboard_expression not in line and pipe_expression not in line:
             print("File read event from Log B: " + line)
             count += 1
-    print(str(count) + " file read events in Log B")
+    b_file_events_count = count
+
+    print(str(a_read_events_count) + " read events in Log A")
+    print(str(b_read_events_count) + " read events in Log B")
+    print(str(a_keyboard_read_events_count) + " keyboard read events in Log A")
+    print(str(b_keyboard_read_events_count) + " keyboard read events in Log B")
+    print(str(a_file_read_events_count) + " file read events in Log A")
+    print(str(b_file_events_count) + " file read events in Log B")
